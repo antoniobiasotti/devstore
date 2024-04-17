19438916e48f00f2e442938687e5d4465cd86852
antoniobiasotti <abamorim36@gmail.com>
2024-04-17 09:06:17 -0300

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,20 +2,23 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"], variable: '--font inter' });
+const inter = Inter({ 
+  subsets: ["latin"], 
+  variable: '--font-inter',
+});
 
 export const metadata: Metadata = {
   title: "devstore",
-};
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={inter.variable}>{children}</body>
+    <html className={inter.variable} lang="pt">
+      <body>{children}</body>
     </html>
   );
 }
